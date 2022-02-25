9ff2f42164833d46128088f0bb5677f69b79bcbd
Jared D. Baker <jbaksta@gmail.com>
2022-02-24 22:32:50 -0700

    small updates

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ const plugin: JupyterFrontEndPlugin<void> = {
 
     const { commands } = app;
 
-    const baseUrl = paths.urls.base;
+    // const baseUrl = paths.urls.base;
     const hubHost = paths.urls.hubHost || '';
     const hubPrefix = paths.urls.hubPrefix || '';
     const hubUser = paths.urls.hubUser || '';
@@ -33,92 +33,81 @@ const plugin: JupyterFrontEndPlugin<void> = {
     const helpUrl = 'https://arc.ucar.edu/knowledge_base/70549913';
     const mainUrl = 'https://jupyterhub.hpc.ucar.edu';
     var hubControlPanelUrl = '';
-    // var serversUrl = '';
-
 
     if (!hubPrefix) {
       deactivateFlag = true;
     } else {
+      // My Servers
       hubControlPanelUrl = hubHost + URLExt.join(hubPrefix,hubUser,'home');
-      // serversUrl = hubHost + URLExt.join(hubPrefix,'hub','home')
     }
 
     if (deactivateFlag) {
       console.log('Should deactivate menu and possibly return early as to not register.')
     }
-
-    console.log('Base URL: ' + baseUrl);
-
-    var command = 'ncar:main';
+    
+    const category = 'NCAR';
+    var command = ''
+    
+    command = 'ncar:main'
     commands.addCommand(command, {
       label: 'JupyterHub Portal',
       caption: 'JupyterHub Portal',
       execute: (args: any) => {
-        console.log(
-          `ncar:menu has been called ${args['origin']}`
-        );
         window.open(mainUrl,'_blank');
       },
     });
 
-    const category = 'NCAR';
-    palette.addItem({
-      command,
-      category,
-      args: { origin: 'from the palette' }
-    });
+    palette.addItem({command, category, args: { origin: 'from the palette'}});
 
     command = 'ncar:ctrl';
     commands.addCommand(command,{ 
       label: 'My Servers',
       caption: 'My Servers',
       execute: (args: any) => {
-        console.log(
-          `ncar:menu has been called ${args['origin']}`
-        );
         window.open(hubControlPanelUrl);
       },
     });
-
-    palette.addItem({
-      command,
-      category,
-      args: { origin: 'from the palette' }
-    });
+    palette.addItem({command, category, args: { origin: 'from the palette'}})
 
     command = 'ncar:help';
     commands.addCommand(command,{
       label: 'NCAR Help',
       caption: 'NCAR Help',
       execute: (args: any) => {
-        console.log(`ncar:help has been called ${args['origin']}`);
-        window.open(helpUrl,'_blank');
+        window.open(helpUrl,'_blank')
       }
-    });
-
-    palette.addItem({
-      command,category,args: { origin: 'from the palette'}
-    });
+    })
+    palette.addItem({command, category, args: { origin: 'from the palette'}})
 
     command = 'ncar:geocat';
     commands.addCommand(command,{
       label: 'NCAR GeoCAT',
       caption: 'NCAR GeoCAT',
       execute: (args: any) => {
-        window.open('https://geocat.ucar.edu/','_blank');
+        window.open('https://geocat.ucar.edu/','_blank')
       }
     });
-    palette.addItem({command,category,args: {origin: 'from the palette'}});
+    palette.addItem({command, category, args: {origin: 'from the palette'}})
 
     command = 'ncar:pythia';
     commands.addCommand(command,{
       label: 'Project Pythia',
       caption: 'Project Pythia',
       execute: (args: any) => {
-        window.open('http://projectpythia.org/','_blank');
+        window.open('http://projectpythia.org/','_blank')
       }
     })
-    palette.addItem({command,category,args: {origin: 'from the palette'}})
+    palette.addItem({command, category, args: {origin: 'from the palette'}})
+
+    command = 'ncar:wrf-python'
+    commands.addCommand(command,{
+      label: 'WRF-Python',
+      caption: 'WRF Python',
+      execute: (args: any) => {
+        window.open('https://wrf-python.readthedocs.io/en/latest/','_blank')
+      }
+    })
+    palette.addItem({command, category, args: {origin: 'from the palette'}})
 
   }
 };
